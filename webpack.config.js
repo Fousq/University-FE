42b4b7e2fc08172d3797f9acba4c22da763a77fc
Zhanbolat Orakbayev <fous363@gmail.com>
2020-07-27 20:39:18 +0600

    Configure for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry:  {
-        app: './src/index.js'
+        authentication: './src/authentication/index.js'
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/authentication/index.html'}),
+        new CleanWebpackPlugin()
+    ]
 }
